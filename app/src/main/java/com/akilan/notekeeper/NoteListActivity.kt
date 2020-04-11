257e7caf306a34b83431653c22ab90afecd0996f
Akilan Arasu <akilanarasu.94@gmail.com>
2020-04-12 00:08:22 +0530

    Added note list screen

---
+++ app/src/main/java/com/akilan/notekeeper/NoteListActivity.kt
@@ -0,0 +1,20 @@
+package com.akilan.notekeeper
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+    }
+}
